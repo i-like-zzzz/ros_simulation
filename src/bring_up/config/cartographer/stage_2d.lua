ecf971d018fd4a25a2f5711b9b72d5f0d52ca564
wenchen <zwc20000422@gmail.com>
2025-11-24 16:35:20 +0800

    修改参数

--- src/bring_up/config/cartographer/stage_2d.lua
+++ src/bring_up/config/cartographer/stage_2d.lua
@@ -36,12 +36,39 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.min_range = 0.1
-TRAJECTORY_BUILDER_2D.max_range = 30.0
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.0
-TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.5
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(30.)
+TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.min_range = 0.3
+TRAJECTORY_BUILDER_2D.max_range = 100.
+TRAJECTORY_BUILDER_2D.min_z = 0.2
+--TRAJECTORY_BUILDER_2D.max_z = 1.4
+--TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.02
+
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 0.5
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 200.
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_range = 50.
+
+--TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.max_length = 0.9
+--TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.min_num_points = 100
+--TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.max_range = 50.
+
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false --是否使用相关性暴力匹配
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 1. --ceres扫匹配中使用点云优化的残差权重
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1. --ceres扫匹配中使用推断器推断的机器人位置作为优化目标，其残差项权重
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 1. --ceres扫描匹配中角度优化残差权重
+
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 12
+
+--TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+--TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = 0.004
+--TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = 1.
+
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 80.
+TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.1
+
+POSE_GRAPH.optimize_every_n_nodes = 160.
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.3
+POSE_GRAPH.constraint_builder.max_constraint_distance = 15.
+POSE_GRAPH.constraint_builder.min_score = 0.48
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.60
 
 return options
